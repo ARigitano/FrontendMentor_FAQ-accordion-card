97f0698cae0dc732393dacf099e83e794b8a39d9
Antoine Rigitano <antoine@rigitano.com>
2021-07-05 12:08:30 +0200

    classlist

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-let toggleAnswer = (id) => {
+const groups = document.querySelectorAll('.questionGroup');
+
+groups.forEach(group => {
+    group.addEventListener("click", () => {
+        group.classList.toggle("active");
+    });
+});
+
+
+
+/*let toggleAnswer = (id) => {
     let answer = document.getElementById(id);
 
     if(answer.style.display == "none") 
     {
         answer.style.display = "block"; 
+        
     }
     else 
     {
         answer.style.display = "none"; 
     }
-}
+}*/
